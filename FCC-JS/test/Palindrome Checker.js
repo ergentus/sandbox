c88646dc87111ae0c78c9616a520d371a1e50a0c
ergentus <ponfable999@gmail.com>
2023-02-13 18:01:13 -0500

    Palindrome Checker -final up

--- FCC-JS/test/Palindrome Checker.js
+++ FCC-JS/test/Palindrome Checker.js
@@ -1,31 +1,16 @@
 function palindrome(str) {
-    let firstHalfWord = '';
-    let lastHalfWord = '';
+
     const newStr = str.replace(/[^A-Za-z0-9]+/g, '').toLowerCase();
 
-    if (newStr.length % 2 === 1) {
-        for (let i = 0; i < (Math.floor(newStr.length / 2)); i++) {
-            firstHalfWord += newStr[i];
+    for (let i = 0; i < newStr.length / 2; i++) {
+        if (!(newStr[i] === newStr[newStr.length - i - 1])) {
+            return false
         }
-        for (let i = newStr.length - 1; i > Math.floor(newStr.length / 2); i--) {
-            lastHalfWord += newStr[i];
-        }
-    } else {
-        for (let i = 0; i < newStr.length / 2; i++) {
-            firstHalfWord += newStr[i];
-        }
-        for (let i = newStr.length - 1; i > newStr.length / 2 - 1; i--) {
-            lastHalfWord += newStr[i];
-        }
-    }
 
-    if (firstHalfWord === lastHalfWord) {
-        return true
     }
-    return false
-
+    return true
 }
 
-const string ="race car"
+const string = "Race car"
 
 console.log(palindrome(string));
