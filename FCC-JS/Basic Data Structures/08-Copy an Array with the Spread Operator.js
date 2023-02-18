b051e37820f2a542cdbf24d593df311954603464
ergentus <ponfable999@gmail.com>
2023-02-18 11:14:34 +0300

    08-solution(array)

--- FCC-JS/Basic Data Structures/08-Copy an Array with the Spread Operator.js
+++ FCC-JS/Basic Data Structures/08-Copy an Array with the Spread Operator.js
@@ -2,7 +2,7 @@ function copyMachine(arr, num) {
    let newArr = [];
    while (num >= 1) {
       // Only change code below this line
-      newArr.push(arr);
+      newArr.push([...arr]);
       // Only change code above this line
       num--;
    }
