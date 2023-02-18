04b6b67cbba1de669660dca5ebe9c0de984592b5
ergentus <ponfable999@gmail.com>
2023-02-19 01:01:03 +0300

    04-solution(basic algorithms)

---
+++ FCC-JS/Basic Algorithm Scripting/04-Find the Longest Word in a String.js
@@ -0,0 +1,6 @@
+function findLongestWordLength(str) {
+   const arr = str.split(' ').map((el) => el.length).sort((a, b) => b - a)
+   return arr[0]
+}
+
+findLongestWordLength("What if we try a super-long word such as otorhinolaryngology");
