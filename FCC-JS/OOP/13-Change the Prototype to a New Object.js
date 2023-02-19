f3549f67ba7b6664fc4cc8d17cdce480808e8e4f
ergentus <ponfable999@gmail.com>
2023-02-20 02:45:46 +0300

    13-solution(OOP)

---
+++ FCC-JS/OOP/13-Change the Prototype to a New Object.js
@@ -0,0 +1,16 @@
+function Dog(name) {
+   this.name = name;
+}
+
+Dog.prototype = {
+   // Only change code below this line
+   numLegs: 4,
+   eat() {
+
+   },
+   describe() {
+
+   }
+};
+
+let taksa = new Dog('Luffy')
