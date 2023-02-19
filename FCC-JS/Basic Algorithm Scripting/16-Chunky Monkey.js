7b2ea3adf7eee9bb4af8c9f1cf80367819a5746a
ergentus <ponfable999@gmail.com>
2023-02-19 11:43:21 +0300

    16-solution(basic algorithms) up

--- FCC-JS/Basic Algorithm Scripting/16-Chunky Monkey.js
+++ FCC-JS/Basic Algorithm Scripting/16-Chunky Monkey.js
@@ -8,3 +8,12 @@ function chunkArrayInGroups(arr, size) {
 }
 
 chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);
+
+
+/* function chunkArrayInGroups(arr, size) {
+   const newArr = [];
+   while (arr.length > 0) {
+      newArr.push(arr.splice(0, size));
+   }
+   return newArr;
+} */ //best solution
