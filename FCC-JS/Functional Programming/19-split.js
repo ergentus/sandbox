fecc36c8c5c42418067db861adb9c969ac11c0f4
ergentus <ponfable999@gmail.com>
2023-02-21 14:04:17 +0300

    19-solution(FP)

---
+++ FCC-JS/Functional Programming/19-split.js
@@ -0,0 +1,9 @@
+function splitify(str) {
+   // Only change code below this line
+   console.log(str.split(/\W/g))
+   return str.split(/\W/g)
+
+   // Only change code above this line
+}
+
+splitify("Hello World,I-am code");
