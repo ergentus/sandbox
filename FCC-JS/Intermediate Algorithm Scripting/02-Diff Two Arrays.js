51950d9a7b9556bed8a17722cd9eab3518eda268
ergentus <ponfable999@gmail.com>
2023-02-23 04:16:22 +0300

    02-solution+up

--- FCC-JS/Intermediate Algorithm Scripting/02-Diff Two Arrays.js
+++ FCC-JS/Intermediate Algorithm Scripting/02-Diff Two Arrays.js
@@ -1,7 +1,24 @@
-function diffArray(arr1, arr2) {
+/*function diffArray(arr1, arr2) {
    return arr1
       .concat(arr2)
       .filter(item => !arr1.includes(item) || !arr2.includes(item));
+}
+*/
+
+function diffArray(arr1, arr2) {
+   let newArr = [];
+   function onlyInFirst(first, second) {
+      first.map((el) => {
+         if (second.indexOf(el) === -1) {
+            newArr.push(el);
+         }
+      })
+   }
+
+   onlyInFirst(arr1, arr2);
+   onlyInFirst(arr2, arr1);
+
+   return newArr;
 }
 
 diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
